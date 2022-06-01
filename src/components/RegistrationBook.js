621a16ac2c161860ab50e988f6ae91aaa23108b0
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-06-01 19:16:54 -0300

    Add RegistrationBook architectural base

---
+++ src/components/RegistrationBook.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const RegistrationBook = () => {
+  return (
+    <>
+      <h2>Register books</h2>
+      <form action="">
+        <input type="text" name="title" placeholder="Insert the title.." />
+        <input type="text" name="author" placeholder="Insert the author.." />
+        <button type="submit">Register</button>
+      </form>
+    </>
+  );
+};
+
+export default RegistrationBook;
