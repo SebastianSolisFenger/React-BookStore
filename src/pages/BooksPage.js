d40629c959a48d4ee0c36288fc2f4b931ce100c0
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-06-01 19:38:26 -0300

    Import BookList and RegistrationBook COMPONENTS into BooksPage

--- src/pages/BooksPage.js
+++ src/pages/BooksPage.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import BooksList from '../components/BooksList';
+import RegistrationBook from '../components/RegistrationBook';
 
-const BooksPage = () => <h2>BooksPage</h2>;
+const BooksPage = () => (
+  <>
+    <h2>BooksPage</h2>
+    <BooksList />
+    <RegistrationBook />
+  </>
+);
 
 export default BooksPage;
