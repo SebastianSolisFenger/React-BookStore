2ca401086934c94a8904259756e8f6d559937888
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-06-01 19:06:39 -0300

    Add Routes and Navbar architectural base

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import links from '../Routes';
 
 const Navbar = () => {
-  return <h2>Navbar</h2>;
+  return (
+    <div>
+      <h1>BookStore</h1>
+      <nav>
+        <ul>
+          {links.map((link) => (
+            <li key={link.id}>
+              <Link to={link.path}>{link.text}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
