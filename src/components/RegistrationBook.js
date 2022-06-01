538cc0056c2e9b1dc4db270a5f2593d790748dea
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-06-01 19:22:53 -0300

    Add missing className to form

--- src/components/RegistrationBook.js
+++ src/components/RegistrationBook.js
@@ -4,7 +4,7 @@ const RegistrationBook = () => {
   return (
     <>
       <h2>Register books</h2>
-      <form action="">
+      <form className="register-form">
         <input type="text" name="title" placeholder="Insert the title.." />
         <input type="text" name="author" placeholder="Insert the author.." />
         <button type="submit">Register</button>
