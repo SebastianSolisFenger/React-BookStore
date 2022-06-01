7d8f95d5869828e5a59f3dbda6f55cb58a5a3cf0
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-06-01 18:47:51 -0300

    Add BooksList achitectural base

---
+++ src/components/BooksList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Book from './Book';
+
+const BooksList = () => {
+  const books = [
+    { id: 1, name: 'The wolf', category: 'Sci-Fi', author: 'Mike Sckot' },
+    { id: 2, name: 'The wolf', category: 'Sci-Fi', author: 'Mike Sckot' },
+    { id: 3, name: 'The wolf', category: 'Sci-Fi', author: 'Mike Sckot' },
+    { id: 4, name: 'The wolf', category: 'Sci-Fi', author: 'Mike Sckot' },
+  ];
+
+  return (
+    <div>
+      <ul>
+        {books.map((book) => (
+          <Book
+            key={book.id}
+            id={book.id}
+            name={book.name}
+            author={book.author}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default BooksList;
