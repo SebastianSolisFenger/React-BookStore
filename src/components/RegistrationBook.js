0d0e1e23c2aa43e4dffd81ed56a1020043100b11
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-06-06 19:01:25 -0300

    Implement useDispatch for adding books

--- src/components/RegistrationBook.js
+++ src/components/RegistrationBook.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
 
-const RegistrationBook = () => (
-  <>
-    <h2>Register books</h2>
-    <form className="register-form">
-      <input type="text" name="title" placeholder="Insert the title.." />
-      <input type="text" name="author" placeholder="Insert the author.." />
-      <button type="submit">Register</button>
-    </form>
-  </>
-);
+const RegistrationBook = () => {
+  const dispatch = useDispatch();
+
+  const registerEventBookFunc = (e) => {
+    e.preventDefault();
+    const { title, author } = e.target.elements;
+    const newBookObj = {
+      title: title.value,
+      author: author.value,
+    };
+    dispatch(addBook(newBookObj));
+  };
+
+  return (
+    <>
+      <h2>Register books</h2>
+      <form className="register-form" onSubmit={registerEventBookFunc}>
+        <input type="text" name="title" placeholder="Insert the title.." />
+        <input type="text" name="author" placeholder="Insert the author.." />
+        <button type="submit">Register</button>
+      </form>
+    </>
+  );
+};
 
 export default RegistrationBook;
