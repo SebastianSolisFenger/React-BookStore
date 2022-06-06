7a0f58e1149ff12728bfe3b3cebe95e606d66c6f
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-06-06 10:10:08 -0300

    Combine reducers in the root and add action types and creators for books and categories

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -5,5 +5,27 @@ const REMOVE_BOOK = 'REMOVE_BOOK';
 
 // ACTION CREATORS
 
-const addBook = (title) => ({ type: ADD_BOOK, payload: { title } });
-const removeBook = (id) => ({ type: REMOVE_BOOK, payload: { id } });
+export const addBook = (book) => ({ type: ADD_BOOK, payload: book });
+export const removeBook = (id) => ({ type: REMOVE_BOOK, payload: { id } });
+
+// REDUCER
+
+const books = (state = [], action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [
+        ...state,
+        {
+          id: state.length,
+          name: action.payload.name,
+          author: action.payload.author,
+        },
+      ];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.payload.id);
+    default:
+      return state;
+  }
+};
+
+export default books;
---
+++ src/redux/categories/categories.js
@@ -0,0 +1,16 @@
+// ACTION TYPES FOR CATEGORIES
+const CHECK_STATUS = 'CHECK_STATUS';
+
+// ACTION CREATORS
+export const checkStatus = () => ({ type: CHECK_STATUS });
+
+const categories = (state = [], action) => {
+  switch (action.type) {
+    case CHECK_STATUS:
+      return 'Under Construction';
+    default:
+      return state;
+  }
+};
+
+export default categories;
