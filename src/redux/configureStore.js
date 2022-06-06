3d9e28516f1a1712c41628e30a07a50038e9e227
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-06-06 18:24:11 -0300

    Add PROVIDER with the STORE exported from redux in CONFIGURATION-STORE.JS

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,6 +2,6 @@ import { combineReducers } from 'react-redux';
 import books from './books/books';
 import categories from './categories/categories';
 
-const root = combineReducers({ books, categories });
+const store = combineReducers({ books, categories });
 
-export default root;
+export default store;
