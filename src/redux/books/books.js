ce77f909ddd87ec30c8ed22d4c14a7ace1763fc7
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-06-07 17:59:41 -0300

    Add THUNK in configureStore and implement actions and action creators for success, failure and loading states

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,31 +1,60 @@
 import { v4 as uuidv4 } from 'uuid';
-import initialBookDataBase from './initialBookDataBase';
+import initialBookStateData from './initialBookStateData';
 
 // ACTION TYPES
 
 const ADD_BOOK = 'BookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'BookStore/books/REMOVE_BOOK';
+const GET_BOOKS_SUCCESS = 'BookStore/books/GET_BOOKS_SUCCESS';
+const GET_BOOKS_FAILURE = 'BookStore/books/GET_BOOKS_FAILURE';
+const GET_BOOKS_LOADING = 'BookStore/books/GET_BOOKS_LOADING';
 
 // ACTION CREATORS
 
 export const addBook = (book) => ({ type: ADD_BOOK, payload: book });
-export const removeBook = (id) => ({ type: REMOVE_BOOK, payload: { id } });
+export const removeBook = (id) => ({ type: REMOVE_BOOK, payload: id });
+export const getBooksSuccess = (books) => ({
+  type: GET_BOOKS_SUCCESS,
+  payload: books,
+});
+export const getBooksFailure = () => ({
+  type: GET_BOOKS_FAILURE,
+});
+export const getBooksLoading = () => ({
+  type: GET_BOOKS_LOADING,
+});
 
 // REDUCER
 
-const books = (state = initialBookDataBase, action) => {
+const books = (state = initialBookStateData, action) => {
   switch (action.type) {
-    case ADD_BOOK:
-      return [
+    case GET_BOOKS_LOADING:
+      return {
         ...state,
-        {
-          id: uuidv4(),
-          title: action.payload.title,
-          author: action.payload.author,
-        },
-      ];
-    case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.payload.id);
+        loading: true,
+      };
+
+    case GET_BOOKS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        books: action.payload,
+      };
+
+    case GET_BOOKS_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+
+    case ADD_BOOK:
+      return {
+        ...state,
+        loading: true,
+        books: [...state.books, action.payload],
+      };
+
     default:
       return state;
   }
