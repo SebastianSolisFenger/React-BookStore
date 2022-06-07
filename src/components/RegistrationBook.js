d96b982d3220d3dd87a2d484de4294854a875743
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-06-07 15:44:33 -0300

    Update based on requirement: "title" and "author" should have their inputs that updates the local state every time they update (onChange)

--- src/components/RegistrationBook.js
+++ src/components/RegistrationBook.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../redux/books/books';
 
 const RegistrationBook = () => {
   const dispatch = useDispatch();
+  const [book, changeBook] = useState({ title: '', author: '' });
+
+  const updateTitle = (e) => {
+    changeBook({
+      ...book,
+      title: e.target.value,
+    });
+  };
+
+  const updateAuthor = (e) => {
+    changeBook({
+      ...book,
+      author: e.target.value,
+    });
+  };
 
   const registerEventBookHandler = (e) => {
     e.preventDefault();
@@ -19,8 +34,18 @@ const RegistrationBook = () => {
     <>
       <h2>Register books</h2>
       <form className="register-form" onSubmit={registerEventBookHandler}>
-        <input type="text" id="title" placeholder="Insert the title.." />
-        <input type="text" id="author" placeholder="Insert the author.." />
+        <input
+          type="text"
+          id="title"
+          placeholder="Insert the title.."
+          onChange={updateTitle}
+        />
+        <input
+          type="text"
+          id="author"
+          placeholder="Insert the author.."
+          onChange={updateAuthor}
+        />
         <button type="submit">Register</button>
       </form>
     </>
