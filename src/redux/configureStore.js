bd48b42e0ffbde26328ec0f7bd86e84755e25290
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-06-07 10:07:42 -0300

    Fix compiling error by importing = legacy_createStore as createStore

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,8 @@
-import { combineReducers } from 'react-redux';
+import { legacy_createStore as createStore, combineReducers } from 'redux';
 import books from './books/books';
 import categories from './categories/categories';
 
-const store = combineReducers({ books, categories });
+const rootReducer = combineReducers({ books, categories });
+const store = createStore(rootReducer);
 
 export default store;
