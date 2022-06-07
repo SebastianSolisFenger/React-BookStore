d9e7c2d5200372f3b709742d5405fcf04cc13ff1
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-06-07 19:04:13 -0300

    Add Category input and modify registerEventBookHandler

--- src/components/RegistrationBook.js
+++ src/components/RegistrationBook.js
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../redux/books/books';
+import { v4 as uuidv4 } from 'uuid';
 
 const RegistrationBook = () => {
   const dispatch = useDispatch();
   const [book, changeBook] = useState({ title: '', author: '' });
+  import { registerNewBook } from '../redux/books/books';
 
   const updateTitle = (e) => {
     changeBook({
@@ -22,12 +24,17 @@ const RegistrationBook = () => {
 
   const registerEventBookHandler = (e) => {
     e.preventDefault();
-    const { title, author } = e.target.elements;
-    const newBookObj = {
+    const { title, author, category } = e.target.elements;
+    const newBook = {
+      item_id: uuidv4(),
       title: title.value,
       author: author.value,
+      category: category.value,
     };
-    dispatch(addBook(newBookObj));
+    dispatch(registerNewBook(newBook));
+    title.value = '';
+    author.value = '';
+    category.value = '';
   };
 
   return (
@@ -39,12 +46,21 @@ const RegistrationBook = () => {
           id="title"
           placeholder="Insert the title.."
           onChange={updateTitle}
+          required
         />
         <input
           type="text"
           id="author"
           placeholder="Insert the author.."
           onChange={updateAuthor}
+          required
+        />
+        <input
+          type="text"
+          id="category"
+          placeholder="Category.."
+          onChange={updateAuthor}
+          required
         />
         <button type="submit">Register</button>
       </form>
