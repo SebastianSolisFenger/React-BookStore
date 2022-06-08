9b22169883fc9548284b1cd373c9b5a1c9c43868
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-06-08 13:48:49 -0300

    Add navbar design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
 import Navbar from './components/Navbar';
 import CategoriesPage from './pages/CategoriesPage';
 import BooksPage from './pages/BooksPage';
--- src/pages/BooksPage.js
+++ src/pages/BooksPage.js
@@ -2,12 +2,21 @@ import React from 'react';
 import BooksList from '../components/BooksList';
 import RegistrationBook from '../components/RegistrationBook';
 
-const BooksPage = () => (
-  <>
-    <h2>BooksPage</h2>
-    <BooksList />
-    <RegistrationBook />
-  </>
-);
+const BooksPage = () => {
+  const hrStyle = {
+    marginTop: '2rem',
+    marginBottom: '1.8rem',
+    border: '1.5px solid var(--grey-border)',
+  };
+
+  return (
+    <div className="page-body">
+      <h2>BooksPage</h2>
+      <BooksList />
+      <hr style={hrStyle} />
+      <RegistrationBook />
+    </div>
+  );
+};
 
 export default BooksPage;
